86e70409926ad64585468de14394845203aa2ee4
Hai <iiRandom.1710@gmail.com>
2019-08-08 20:16:11 +1000

    update, switch to paper

--- exec.bat
+++ exec.bat
@@ -1,1 +1,1 @@
-java -Xmx4096M -Xms4096M -jar server.jar -nogui
+java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -jar server.jar -nogui
